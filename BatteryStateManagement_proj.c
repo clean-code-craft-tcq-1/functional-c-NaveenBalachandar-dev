48f19dc24d4c74b6354addae6d70e88296281612
NaveenBalachandar-dev <77613377+NaveenBalachandar-dev@users.noreply.github.com>
2021-03-02 21:23:19 +0530

    Update BatteryStateManagement_proj.c
    Function splitup

--- BatteryStateManagement_proj.c
+++ BatteryStateManagement_proj.c
@@ -8,21 +8,48 @@
 #include <stdio.h>
 #include <assert.h>
 
-int batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
+int batteryTempMonitor_i(float temperature)
+{
+  if(temperature < 0 || temperature > 45)
+  {
     printf("Temperature out of range!\n");
     return 0;
-  } else if(soc < 20 || soc > 80) {
+  }
+  return 1;
+}
+
+int batterySocMonitor_i(float soc) 
+{
+  if(soc < 20 || soc > 80) 
+  {
     printf("State of Charge out of range!\n");
     return 0;
-  } else if(chargeRate > 0.8) {
+  }
+  return 1;
+}
+
+int batteryChargerateMonitor_i(float chargeRate)
+{
+  if(chargeRate > 0.8)
+  {
     printf("Charge Rate out of range!\n");
     return 0;
   }
   return 1;
 }
 
-int main() {
+int batteryIsOk(float temperature, float soc, float chargeRate)
+{ 
+ int retTempStat_i, retsocStat_i,retchargeStat_i;
+  
+  retTempStat_i   = batteryTempMonitor_i(temperature);
+  retsocStat_i    = batterySocMonitor_i(soc);
+  retchargeStat_i = batteryChargerateMonitor_i(chargeRate);
+  
+  return ((retTempStat_i|retsocStat_i)|retchargeStat_i);
+}
+
+void main() {
   assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0));
 }
